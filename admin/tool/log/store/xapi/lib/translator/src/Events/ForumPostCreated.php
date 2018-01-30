8fef5b563238112549d8314bba5fb99de5e7ddd9
hecking <hecking@collide.info>
2018-01-30 10:10:46 +0100

    Fixed issue with object name.

--- admin/tool/log/store/xapi/lib/translator/src/Events/ForumPostCreated.php
+++ admin/tool/log/store/xapi/lib/translator/src/Events/ForumPostCreated.php
@@ -22,10 +22,11 @@ class ForumPostCreated extends ModuleViewed {
 
             'post_url' => $opts['discussion']->url . '#' . $opts['post']->id,
             'post_discussion_name' => $opts['discussion']->name,
+            'post_name' => 'Forum post',
             'post_description' => 'A forum post in a Moodle course.',
             'post_type' => (($opts['post']->parent > 0) ? 'http://id.tincanapi.com/activitytype/forum-topic' : 'http://id.tincanapi.com/activitytype/forum-reply'),
             'post_ext' => $opts['post'],
-            'discussion_ext_key' => 'http://inf.uni-due.de/define/extensions/moodle_forum_post'
+            'post_ext_key' => 'http://inf.uni-due.de/define/extensions/moodle_forum_post'
         ])];
     }
 }

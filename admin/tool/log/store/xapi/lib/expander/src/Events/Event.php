7905b4422e6293296c07aa6d4aae2636a36379f7
hecking <hecking@collide.info>
2018-05-08 09:58:52 +0200

    Group information will be added to the context field in xAPI statements.

--- admin/tool/log/store/xapi/lib/expander/src/Events/Event.php
+++ admin/tool/log/store/xapi/lib/expander/src/Events/Event.php
@@ -38,10 +38,7 @@ class Event extends PhpObj {
      * @return [String => Mixed]
      */
     public function read(array $opts) {
-        $groups = groups_get_all_groups($opts['courseid'], $opts['userid']);
-        $fp = fopen('vardump.txt', 'w');
-        fwrite($fp, $opts['userid'] . " " . count($groups) . " : ". serialize($groups) . "\n\n");
-        fclose($fp);
+
         return [
             'user' => $opts['userid'] < 1 ? null : $this->repo->read_user($opts['userid']),
             'relateduser' => $opts['relateduserid'] < 1 ? null : $this->repo->read_user($opts['relateduserid']),
